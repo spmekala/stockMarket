84df4dca73be8dce8cbb751616420776e66952c4
spmekala <spmekala@gmail.com>
2017-10-20 15:55:59 -0400

    Added Maven dependencies for Quandl and fixed Junit Dependencies

--- src/test/java/com/rbsm/AppTest.java
+++ src/test/java/com/rbsm/AppTest.java
@@ -1,14 +1,11 @@
 package com.rbsm;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
