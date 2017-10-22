e28899c4fd665be7f474b7bbacf785e5638b008e
rbhat35 <rsbhat2@gmail.com>
2017-10-22 10:25:37 -0400

    Added first financial data set

--- src/main/java/com/rbsm/App.java
+++ src/main/java/com/rbsm/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ClassicQuandlSession session = ClassicQuandlSession.create(SessionOptions.Builder.withAuthToken().build());
+        ClassicQuandlSession session = ClassicQuandlSession.create(SessionOptions.Builder.withAuthToken("-R5snzzsHJ9iSN4xjgSv").build());
         TabularResult tabularResult = session.getDataSet(
                 DataSetRequest.Builder.of("WIKI/AAPL").build());
         System.out.println(tabularResult.toPrettyPrintedString());
