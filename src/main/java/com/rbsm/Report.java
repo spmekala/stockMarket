3e82b03d790c3086769cc6b2a62c1715f003ceae
rbhat35 <rsbhat2@gmail.com>
2017-10-22 11:32:26 -0400

    Added report class

---
+++ src/main/java/com/rbsm/Report.java
@@ -0,0 +1,15 @@
+package com.rbsm;
+
+import java.util.HashMap;
+
+public class Report {
+
+    public String adsh;
+    public String name;
+    public HashMap<String, Double> data;
+
+    public Report(String adsh, String name) {
+        this.adsh = adsh;
+        this.name = name;
+    }
+}
